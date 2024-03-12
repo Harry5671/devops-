76345c4ba50d5a0d7b95e88201ac060c6f73fb1c
Harry5671 <143925139+Harry5671@users.noreply.github.com>
2024-03-12 10:58:18 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
    
 docker login -u harry35c -p dckr_pat_Ac5fmhM4sbct_gYCEUlRPSS1h_g
-docker tag web:latest harry35c/dev
-docker push harry35c/dev
+docker tag web:latest harry35c/prod
+docker push harry35c/prod
